a9eeadad3b9f2ee3c857e5ad633be755395f3f5c
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2017-08-02 17:05:55 -0600

    Sintetizador Adaptativo

---
+++ Adaptive_Synth/Adaptive_Synth.ino
@@ -0,0 +1,69 @@
+//Tomas de Camino Beck
+//Polymathboxes
+
+#include <Wire.h>
+#include "Chromosome.h"
+#include <Adafruit_MCP4725.h>
+
+#define N 10
+#define POT1 A0
+#define POT2 A3
+#define MIC1 A2
+#define D 200
+
+
+Adafruit_MCP4725 dac;
+
+population pop(N);
+
+float x = 0;
+int val2;
+long loud = 0;
+
+long lastMillis = millis();
+
+
+void setup(void) {
+  Serial.begin(9600);
+  dac.begin(0x62);
+
+  pinMode(POT1, INPUT);
+  pinMode(POT2, INPUT);
+  pinMode(MIC1, INPUT);
+}
+
+void loop(void) {
+  loud = 0;
+  val2 = map(analogRead(POT1), 0, 1023, 1, 16000);
+  // Push out the right lookup table, depending on the selected resolution
+  pop.copyCrossover(2);
+  pop.mutateChromosomes(0.05, 1);
+  evaluate();
+  pop.sort();
+  play(pop.chromosome[0], D*10);  
+}
+
+void play(int pitch, int tempo) {
+  loud = 0;
+  for (int i = 0; i < tempo; i++) {
+    int w = round((2047 * sin(TWO_PI * x) + 2047));
+    dac.setVoltage(w, false);
+    //Serial.println(w);
+    x += pitch;
+    delayMicroseconds(val2);
+    loud += analogRead(MIC1);
+  }
+}
+
+
+void fitness(int a) {
+  pop.fitness[a] = loud / 7500;
+}
+
+//Evalua cada cromosoma
+void evaluate() {
+  for (int i = 0; i < pop.n; i++) {
+    play(pop.chromosome[i], D);
+    fitness(i);
+  }
+}
---
+++ Adaptive_Synth/Chromosome.h
@@ -0,0 +1,159 @@
+/*Tomas de Camino Beck
+
+  Class Chromosome to use in a genetic algorithm
+  It creates a population of chromosomes.  The class
+  allows for crhomosome mutation and crossover operations.
+  Fitness evaluation has to be programmed in the main code
+
+  Best fit chromosome is assumed to be chromosome 0
+
+  Ussage:
+  1. create a objet class chromosome initialize population of n
+  2. Create a fitness and evaluation functions (in main code)
+  3. use mutation and crossover over class crhomosome
+  4. Evaluate fitness
+  5. use sort to rank, highest fitness in chromosome 0
+  6. Choose best fit (in main code) then loop to 3
+
+  Made for teaching poruposes www.funcostarica.org
+*/
+//**************Class population*************
+class population
+{
+
+  public:
+    //
+    byte* chromosome;
+    float* fitness;
+    int n; //numero de entradas
+
+    //creates a population of chromosomes
+    population(int ind) {
+      n = ind;
+      chromosome = new byte[n];
+      fitness = new float[n];
+    }
+
+    //randomize the initial population
+    randomize() {
+      for (int i = 0; i < n; i++) {
+        chromosome[i] = random(255);
+      }
+
+    }
+
+    //uses bubble sort, sorting by fitness
+    void sort()
+    {
+      int i, j;
+      for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+          if (fitness[j] < fitness[j + 1]) {
+            //swap fitnes
+            float f = fitness[j];
+            fitness[j] = fitness[j + 1];
+            fitness[j + 1] = f;
+            //swap chromosome
+            byte ind = chromosome[j];
+            chromosome[j] = chromosome[j + 1];
+            chromosome[j + 1] = ind;
+          }
+        }
+      }
+    }
+
+    //function to read a particular gene of chromosome 0
+    byte readGene(int pos) {
+      return bitRead(chromosome[0], pos);
+
+    }
+
+    //function to read a particular gene of chromosome ind
+    byte readGene(int ind, int pos) {
+      return bitRead(chromosome[ind], pos);
+
+    }
+
+    //counts the number of bits ON of chromosome a
+    int countBits(int a) {
+      byte count = 0;
+      for (int i = 0; i < 8; i++)
+      {
+        count += bitRead(chromosome[a], i);
+      }
+      return count;
+    }
+
+    //** Replicaciton, mutation and crossover functions
+
+    //copy chromosome 0
+    copyChromosomes() {
+      for (int i = 1; i < n; i++) {
+        chromosome[i] = chromosome[0];
+      }
+
+    }
+
+    //mutate genes with probability p
+    mutateChromosomes(float p) {
+      int prop = int(p * 10000);//to use random as rulete
+      for (int i = 1; i < n; i++) {
+        for (int j = 0; j < 7; j++)
+        {
+          if (random(10000) < prop)
+            //changes bit
+            bitWrite(chromosome[i], j, !bitRead(chromosome[i], j));
+        }
+      }
+
+    }
+
+    //mutate genes with probability p, starting from first
+    mutateChromosomes(float p, int first) {
+      int prop = int(p * 10000);//to use random as rulete
+      for (int i = first; i < n; i++) {
+        for (int j = 0; j < 7; j++)
+        {
+          if (random(10000) < prop)
+            //changes bit
+            bitWrite(chromosome[i], j, !bitRead(chromosome[i], j));
+        }
+      }
+
+    }
+
+    //crossover between cromosome a and b
+    //returns a new chromosome
+    byte crossover(int a, int b) {
+      byte cutPos1 = random(4);
+      byte cutPos2 = random(4, 7);
+
+      byte indA = chromosome[a];
+      for (int i = cutPos1; i < cutPos2; i++)
+      {
+        bitWrite(indA, i, bitRead(chromosome[b], i));
+      }
+      return indA;
+    }
+
+
+    //crossover betwwen de first m chromosomes
+    //copies the result to the rest of the population
+    copyCrossover(int m) {
+      int r = 0;
+      for (int i = m; i < n; i++) {
+        int cA = random(m);
+        int cB = random(m);
+        chromosome[i] = crossover(cA, cB);
+      }
+
+    }
+
+
+
+  private:
+    //
+
+
+};
+
